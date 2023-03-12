d07c5353f3971addfdb8e319b285e60d6b7c93f8
Olga <olya.labkovich@mail.ru>
2023-03-12 08:38:38 +0300

    add func hello_every_second

--- asyncio_tut/func_asyncio.py
+++ asyncio_tut/func_asyncio.py
@@ -15,13 +15,19 @@ async def add_number_2(number: int) -> int:
     return number + 2
 
 
+async def hello_every_second():
+    for i in range(2):
+        await asyncio.sleep(1)
+        print('I am sleeping')
+
+
 async def main() -> None:
     sleep_for_three = asyncio.create_task(delay(3))
     sleep_again = asyncio.create_task(delay(3))
     sleep_again_2 = asyncio.create_task(delay(3))
-
+    await hello_every_second()
     await sleep_for_three
-    await sleep_for_three
+    await sleep_again
     await sleep_again_2
 
 
