52ea3befb868932ac98f90d52fe0e68d7c9e96a9
Olga <olya.labkovich@mail.ru>
2023-03-12 20:04:07 +0300

    add delay func to code

--- asyncio_tut/count_code.py
+++ asyncio_tut/count_code.py
@@ -5,7 +5,7 @@ from func import delay, async_timed
 @async_timed()
 async def cpu_bound_work() -> int:
     counter = 0
-    for i in range(100_000_000):
+    for i in range(10_000_000):
         counter += 1
     return counter
 
@@ -14,7 +14,8 @@ async def cpu_bound_work() -> int:
 async def main():
     task_one = asyncio.create_task(cpu_bound_work())
     task_two = asyncio.create_task(cpu_bound_work())
-
+    task_three = asyncio.create_task(delay(3))
+    await task_three
     await task_one
     await task_two
 
--- asyncio_tut/func.py
+++ asyncio_tut/func.py
@@ -4,13 +4,6 @@ import time
 from typing import Callable, Any
 
 
-async def delay(delay_seconds: int) -> int:
-    print(f'Я засыпаю на {delay_seconds} минут')
-    await asyncio.sleep(delay_seconds)
-    print(f'Сон в течение {delay_seconds} закончился')
-    return delay_seconds
-
-
 def async_timed():
     def wrapper(func: Callable) -> Callable:
         @functools.wraps(func)
@@ -23,5 +16,26 @@ def async_timed():
                 end = time.time()
                 total = end - start
                 print(f'{func} завершилась {total:.4f} c')
+
         return wrapped
+
     return wrapper
+
+
+@async_timed()
+async def delay(delay_seconds: int) -> int:
+    print(f'Я засыпаю на {delay_seconds} минут')
+    await asyncio.sleep(delay_seconds)
+    print(f'Сон в течение {delay_seconds} закончился')
+    return delay_seconds
+
+@async_timed()
+async def main():
+    task_one = asyncio.create_task(delay(2))
+    task_two = asyncio.create_task(delay(3))
+
+    await task_one
+    await task_two
+
+if __name__ == '__main__':
+    asyncio.run(main())
